e912199802af67970ca62f9028cf856e0499d980
Felipe Moura <felipecandidodemoura@gmail.com>
2024-12-13 23:43:07 -0300

    Parametrizacao dos testes de calculo da aliquota

--- src/tst/TesteCalculoAliquota.java
+++ src/tst/TesteCalculoAliquota.java
@@ -5,33 +5,61 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import app.IRPF;
 
-/**
- *
-* @author moura
-*/
+
+@RunWith(Parameterized.class)
 public class TesteCalculoAliquota {
     IRPF irpf;
 
-    @Before
-    public void setup() {
-        irpf= new IRPF();
+    private float sal, alu, bol, prev, pen;
+    private double res;
+
+    public TesteCalculoAliquota(float salario, float aluguel, float bolsa, float previdencia, float pensao, double result){
+        this.sal = salario;
+        this.alu = aluguel;
+        this.bol = bolsa;
+        this.prev = previdencia;
+        this.pen = pensao;
+        this.res = result;
+
     }
     
+    @Before
+    public void setup() {
+	}
+    
+    @Parameters
+	public static Collection<Object[]> getParameters() {
+		Object[][] parametros = new Object[][] {
+			{8000, 2000, 1500, 1500, 1500, 9.76},
+			{38000, 12000, 1500, 3000, 5000, 21.20}
+
+		};
+		
+		return Arrays.asList(parametros);
+	}
+
     @Test
     public void TesteCalcularAliquota() {
-        irpf.criarRendimento("Salario", true, 8000);
-        irpf.criarRendimento("Aluguel", true, 2000);
-        irpf.criarRendimento("Bolsa", false, 1500);
+        irpf= new IRPF();
+        irpf.criarRendimento("Salario", true, sal);
+        irpf.criarRendimento("Aluguel", true,  alu);
+        irpf.criarRendimento("Bolsa", false,  bol);
 
         irpf.cadastrarDependente("Pedro", "Filho");
-        irpf.cadastrarContribuicaoPrevidenciaria(1500);
-        irpf.cadastrarPensaoAlimenticia("Pedro", 1500);
-        assertEquals(9.76, irpf.getAliquotaEfetiva(), 0.01);
+        irpf.cadastrarContribuicaoPrevidenciaria(prev);
+        irpf.cadastrarPensaoAlimenticia("Pedro",  pen);
+        assertEquals(res, irpf.getAliquotaEfetiva(), 0.01);
     }
 
 }
