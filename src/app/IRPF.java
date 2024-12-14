8a1601b739795e343e1b844f2dd0a883b891144b
Felipe Moura <felipecandidodemoura@gmail.com>
2024-12-13 22:05:13 -0300

    Implementacao do calculo do imposto em si,  e teste unitario inicial frmiza <mizzaelsantos1@gmail.com>
    Co-authored-by: diogjunior100 <diogjunior10071@gmail.com>
    Co-authored-by: Matheus Pimentel Leal <matheusp.eng@gmail.com>
    Co-authored-by: frmiza <mizzaelsantos1@gmail.com>

--- src/app/IRPF.java
+++ src/app/IRPF.java
@@ -310,15 +310,36 @@ public class IRPF {
 		float base, imposto;
 		imposto = 0;
 		base = this.getBaseCalculo();
-		if (base > 2259.20 && base < 2826.65) {
+		if (base > 2259.20) {
 			if (base > 2826.65) {
-				imposto += 567.45;
+				imposto += 42.56;
 
+			}else{
+				imposto += (base - 2259.20) * 0.075;
 			}
-			
-		} 
+		}
+		if (base > 2826.66) {
+			if (base > 3751.05) {
+				imposto += 138.66;
 
-		return valorTotalImposto;
+			}else{
+				imposto += (base - 2826.66) * 0.15;
+			}
+		}
+		if (base > 3751.06) {
+			if (base > 4664.68) {
+				imposto += 205.57;
+			}else{
+				imposto += (base - 3751.06) * 0.225;
+			}
+		}
+		if (base > 4664.68) {
+			
+			imposto += (base - 4664.68) * 0.275;
+		}
+
+
+		return imposto;
 	}
 
 	/**
