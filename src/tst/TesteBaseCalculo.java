abcbdbfa82be44bcf83f2161cd1e7b7d5dbc8703
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2025-01-21 14:28:58 -0300

    Correção TP1: Correção final parametrização testes
    Próximo passo será realizar a correção da triangulação de testes nos testes de base de cálculo

--- src/tst/TesteBaseCalculo.java
+++ src/tst/TesteBaseCalculo.java
@@ -18,24 +18,30 @@ public class TesteBaseCalculo {
 
     private IRPF irpf;
     private String[] rendimentos;
-    private boolean[] rendimentosTributaveis;
     private float[] valoresRendimentos;
+    private boolean[] rendimentosTributaveis;
+    private String[] nomesDeducoes;
+    private float[] valoresDeducoes;
     private String[] dependentes;
     private String[] tiposDependentes;
-    private float contribuicaoPrevidenciaria;
-    private float pensaoAlimenticia;
+    private float[] valoresPensaoAlim;
+    private float[] contribuicoesPrevidenciaria;
     private double baseCalculo;
 
-    public TesteBaseCalculo(String[] rendimentos, boolean[] rendimentosTributaveis, float[] valoresRendimentos,
-            String[] dependentes, String[] tiposDependentes, float contribuicaoPrevidenciaria, float pensaoAlimenticia, double baseCalculo) {
+    public TesteBaseCalculo(String[] rendimentos, float[] valoresRendimentos, boolean[] rendimentosTributaveis,
+        String[] nomesDeducoes, float[] valoresDeducoes, String[] dependentes,
+        String[] tiposDependentes, float[] valoresPensaoAlim, float[] contribuicoesPrevidenciaria,
+        float baseCalculo) {
+
         this.rendimentos = rendimentos;
         this.rendimentosTributaveis = rendimentosTributaveis;
         this.valoresRendimentos = valoresRendimentos;
         this.dependentes = dependentes;
         this.tiposDependentes = tiposDependentes;
-        this.contribuicaoPrevidenciaria = contribuicaoPrevidenciaria;
-        this.pensaoAlimenticia = pensaoAlimenticia;
+        this.valoresPensaoAlim = valoresPensaoAlim;
+        this.contribuicoesPrevidenciaria = contribuicoesPrevidenciaria;
         this.baseCalculo = baseCalculo;
+
     }
 
     @Parameters(name = "{index}: {0}")
@@ -46,35 +52,53 @@ public class TesteBaseCalculo {
             {"RendimentoZ", "RendimentoW"},
             {"RendimentoV"}
         };
+        float[][] valorRendimentos = {
+            {8000.0f, 2000.0f, 1500.0f},
+            {1.0f},
+            {-1000.0f},
+            {10500.0f}
+        };
         boolean[][] rendimentosTributaveis = {
             {true, true, true},
             {true},
             {false, false},
             {true}
         };
-        float[][] valorRendimentos = {
-            {8000.0f, 2000.0f, 1500.0f},
-            {1.0f},
-            {-1000.0f},
-            {10500.0f},
+        String[][] nomesDeducoes = {
+            {"Deducao1", "Deducao2", "Deducao3"},
+            {"Deducao4", "Deducao5", "Deducao6"},
+            {"Deducao7", "Deducao8", "Deducao9"},
+            {"Deducao10", "Deducao11", "Deducao12"}
+        };
+        float[][] valoresDeducoes = {
+            {170.0f, 900.0f, 150.0f, 110.0f},
+            {110.0f, 785.0f, 225.0f, 115.0f},
+            {140.0f, 560.0f, 523.0f, 178.0f},
+            {270.0f, 1000.0f, 980.0f, 198.0f}
         };
         String[][] nomesDependentes = {
-            {"Pedro", "Marcelo", "Henrique"},
-            {"João"},
+            {"Eduardo", "Marcelo", "Henrique"},
+            {"João", "Jhosefer"},
             {"Mateus"},
             {"Walace"}
         };
         String[][] parentescosDependentes = {
             {"Filho", "Neto", "Sobrinho"},
-            {"Sobrinho"},
+            {"Sobrinho", "Filho"},
             {"Filho"},
-            {"Pai"},
+            {"Pai"}
         };
-        float[] contribPrevidenciaria = {
-            1000.0f, 0.0f, -500.0f, 2540.0f
+        float[][] valoresPensaoAlim = {
+            {950.0f, 0.0f, 0.0f},
+            {0.0f, 895.95f},
+            {1325.50f},
+            {0.0f},
         };
-        float[] pensoesAlim = {
-            500.0f, 0.0f, -200.0f, 2500.0f
+        float[][] contribPrevidenciaria = {
+            {1000.0f, 100.0f, 500.0f, 1540.0f},
+            {2000.0f, 200.0f, 600.0f, 2340.0f},
+            {3000.0f, 300.0f, 400.0f, 2100.0f},
+            {4000.0f, 400.0f, 300.0f, 2540.0f}
         };
         float[] basesCalculo = {
             5500.0f, 1.0f, 0.0f, 7770.41f
@@ -83,42 +107,50 @@ public class TesteBaseCalculo {
         return Arrays.asList(new Object[][]{
             {
                 nomesRendimentos[0],
-                rendimentosTributaveis[0],
                 valorRendimentos[0],
+                rendimentosTributaveis[0],
+                nomesDeducoes[0],
+                valoresDeducoes[0],
                 nomesDependentes[0],
                 parentescosDependentes[0],
+                valoresPensaoAlim[0],
                 contribPrevidenciaria[0],
-                pensoesAlim[0],
                 basesCalculo[0]
             },
             {
                 nomesRendimentos[1],
-                rendimentosTributaveis[1],
                 valorRendimentos[1],
+                rendimentosTributaveis[1],
+                nomesDeducoes[1],
+                valoresDeducoes[1],
                 nomesDependentes[1],
                 parentescosDependentes[1],
+                valoresPensaoAlim[1],
                 contribPrevidenciaria[1],
-                pensoesAlim[1],
                 basesCalculo[1]
             },
             {
                 nomesRendimentos[2],
-                rendimentosTributaveis[2],
                 valorRendimentos[2],
+                rendimentosTributaveis[2],
+                nomesDeducoes[2],
+                valoresDeducoes[2],
                 nomesDependentes[2],
                 parentescosDependentes[2],
+                valoresPensaoAlim[2],
                 contribPrevidenciaria[2],
-                pensoesAlim[2],
                 basesCalculo[2]
             },
             {
                 nomesRendimentos[3],
-                rendimentosTributaveis[3],
                 valorRendimentos[3],
+                rendimentosTributaveis[3],
+                nomesDeducoes[3],
+                valoresDeducoes[3],
                 nomesDependentes[3],
                 parentescosDependentes[3],
+                valoresPensaoAlim[3],
                 contribPrevidenciaria[3],
-                pensoesAlim[3],
                 basesCalculo[3]
             },
         });
@@ -127,25 +159,33 @@ public class TesteBaseCalculo {
     @Before
     public void setup() {
         irpf = new IRPF();
+        
         for (int i = 0; i < rendimentos.length; i++) {
             irpf.criarRendimento(rendimentos[i], rendimentosTributaveis[i], valoresRendimentos[i]);
         }
-    }
 
-    @Test
-    public void testCalculoBaseCalculo() {
-
+        for (int i = 0; i < nomesDeducoes.length; i++) {
+            irpf.cadastrarDeducaoIntegral(nomesDeducoes[i], valoresDeducoes[i]);
+        }
 
         for (int i = 0; i < dependentes.length; i++) {
             irpf.cadastrarDependente(dependentes[i], tiposDependentes[i]);
         }
 
-        irpf.cadastrarContribuicaoPrevidenciaria(contribuicaoPrevidenciaria);
-        irpf.cadastrarPensaoAlimenticia(dependentes[0], pensaoAlimenticia);
-        
-        System.out.println("Base calculada pelo método: " + irpf.getBaseCalculo());
-        System.out.println("Valor esperado: " + baseCalculo);
+        for (int i = 0; i < dependentes.length; i++) {
+            irpf.cadastrarPensaoAlimenticia(dependentes[i], valoresPensaoAlim[i]);
+        }
 
-        assertEquals(baseCalculo, irpf.getBaseCalculo(), 0.01);
+        for (int i = 0; i < contribuicoesPrevidenciaria.length; i++) {
+            irpf.cadastrarContribuicaoPrevidenciaria(contribuicoesPrevidenciaria[i]);
+        }
+    }
+
+    @Test
+    public void testCalculoBaseCalculo() {
+        // System.out.println("Base calculada pelo método: " + irpf.getBaseCalculo());
+        // System.out.println("Valor esperado: " + baseCalculo);
+
+        assertEquals(baseCalculo, irpf.getBaseCalculo(), 0.01f);
     }
 }
